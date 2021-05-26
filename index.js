b376ab3b816d6bccb0601c4cc9c7e0be9607d93c
zj <pateertang@sina.com>
2021-05-26 17:27:57 +0800

    aa

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const program = require('commander');
 var process1 = require('child_process');
 //version 版本号
 //name 新项目名称
-program.version('1.0.0', '-v, --version')
-  .command('init <xt-react-cli>')
+program.version('1.0.2')
+  .command('init <xt-react-web-webpack5>')
   .action((name) => {
     console.log('clone template ...');
-    process1.exec('git clone (git 下载地址) ' + name, function (error, stdout, stderr) {
+    process1.exec('https://github.com/catzjzj/xt-react-cli.git' + name, function (error, stdout, stderr) {
       if (error !== null) {
         console.log('exec error: ' + error);
         return;
